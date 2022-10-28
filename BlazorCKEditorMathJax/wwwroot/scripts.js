1a0bddeff488f838b4cf01cd09a443a1c4680c2b
hjyanghj <hjyanghj@gmail.com>
2022-10-28 22:32:16 +0800

    vector.html MathJax + CKEditor OK for \overrightarrow, \overline, \hspace。But  MathJax + CKEditor + Blazor NOT perfect

---
+++ BlazorCKEditorMathJax/wwwroot/scripts.js
@@ -0,0 +1,9 @@
+﻿export function typeset() {
+    MathJax.typeset();
+}
+export async function typeset2(texstring) {
+    var ele = document.createElement("div"); // document.getElementById("me");
+    ele.innerHTML = texstring;
+    await MathJax.typesetPromise([ele]);
+    return ele.innerHTML;
+}
